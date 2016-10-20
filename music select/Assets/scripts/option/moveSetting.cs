0c2258f51089765184564e947423527b1592029a
reika makisoto <reika makisoto>
2016-10-21 00:02:45 +0900

    スピード・難易度の表示等どうしましょう？

---
+++ music select/Assets/scripts/option/moveSetting.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveSetting : MonoBehaviour {
+
+    private Vector3 pos;
+
+    private int difficulty = 2;
+
+    private int speed = 4;
+
+    // Use this for initialization
+    void Start () {
+        pos = transform.position;
+        /*難易度と整数値の対応
+            1 : easy
+            2 : normal
+            3 : hard
+        */
+        
+    }
+    //5.325023 -> -9.04
+
+    // Update is called once per frame
+    void Update () {
+
+        pos = transform.position;
+
+        if (pos.x > 5.32f && pos.x < 5.33f)
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                if (difficulty < 3)
+                {
+                    difficulty++;
+                }
+                else
+                {
+                    difficulty = 1;
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.H))
+            {
+                if (difficulty > 1)
+                {
+                    difficulty--;
+                }
+                else
+                {
+                    difficulty = 3;
+                }
+            }
+            
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                iTween.MoveTo(this.gameObject, iTween.Hash("x", -9.04f));
+            }
+            print("speed:" + speed + " difficulty:" + difficulty);
+        }else {
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                if (speed < 12)
+                {
+                    speed++;
+                }
+                else
+                {
+                    //なにもしない
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.K))
+            {
+                if (speed > 1)
+                {
+                    speed--;
+                }
+                else
+                {
+                    //なにもしない
+                }
+            }
+            print("speed:" + speed + " difficulty:" + difficulty);
+        }
+        transform.position = pos;
+
+	}
+}
---
+++ music select/Assets/scripts/select/music.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System;
+
+public class music : MonoBehaviour {
+
+    //オブジェクトの位置
+    private Vector3 pos;
+
+    //オブジェクトの停止位置のx座標
+    private float stop_right;
+
+    //デバッグ用変数
+    private int num;
+    
+
+    // Use this for initialization
+    void Start ()
+    {
+        pos = transform.position;
+        print(pos);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+        transform.position = pos;
+
+        pos = transform.position;
+
+        //Hが押されたら左に、Lが押されたら右に移動
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            //print("KeyDown : H");
+            if (AddScript.first_pos_x+1>pos.x && pos.x>AddScript.first_pos_x-1)
+            {
+                //左端に来たら右端に移動
+                //不具合回避のため、条件をおおざっぱにしています
+                pos.x = AddScript.first_pos_x + AddScript.dist * (AddScript.numOfMusics - 1);
+
+            }
+            else
+            {
+                //演出用
+                iTween.MoveTo(gameObject, iTween.Hash("x", pos.x - AddScript.dist, "time", 0.763));
+
+            }
+            transform.position = pos;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            print("KeyDown : L");
+            if ((pos.x + AddScript.dist) > (stop_right+1))
+            {
+                //右端に来たら左端に移動
+                pos.x = AddScript.first_pos_x;
+            }
+            else
+            {
+                //演出用
+                iTween.MoveTo(gameObject, iTween.Hash("x", pos.x + AddScript.dist, "time", 0.763));
+            }
+
+                transform.position = pos;
+
+        }
+        //print("At " + num + ", the position : " + pos.x);
+
+    }
+
+    public void StopPosition(float stop_r, int i) {
+        stop_right = stop_r;
+        num = i;
+    }
+
+}
